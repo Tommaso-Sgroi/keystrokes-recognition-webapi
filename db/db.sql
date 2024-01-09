663a37cccf0cac70c6dff79d842752e4d5b4f2ad
Tommaso Sgroi <sgroi.1852992@studenti.uniroma1.it>
2024-01-09 18:25:12 +0100

    Add database

---
+++ db/db.sql
@@ -0,0 +1,19 @@
+create table user
+(
+    ID   int unsigned auto_increment
+        primary key,
+    name tinytext null
+);
+
+create table probe
+(
+
+    keystroke json                     not null comment 'keystrokes JSON, it have a fixed size',
+    userid    int unsigned             not null,
+    keystrokesID      int unsigned auto_increment primary key comment 'keystrokesID',
+    FA        int unsigned default '0' not null comment 'false acceptance',
+    FR        int unsigned default '0' not null comment 'false rejection',
+    constraint probe_user_ID_fk
+        foreign key (userid) references user (ID)
+            on update cascade on delete cascade
+) comment 'users probes of the keystrokes'
