2afb0abd52c208560f87da09d304cb4840726f50
nunzia-damb <nunzy00@gmail.com>
2024-01-11 23:12:19 +0100

    changes
    added new svelte page to insert phrases and a way to register - only fe

--- demo-app/src/routes/+page.svelte
+++ demo-app/src/routes/+page.svelte
@@ -1,48 +1,99 @@
-<div class="title"><h4>Keystroke Identity</h4></div>
+
+{#if visible}
+<div class="title"><h4>Keystroke Identification</h4></div>
+{/if}
+{#if !visible}
+<div class="title"><h4>Keystroke Enrollment</h4></div>
+{/if}
 <div class="bigcard">
-  <div class=container>
-    <h2><b>Claim Id</b></h2>
-  </div>
-  <div class="tab1cards">
-    <div class="container profile">
-      <img class="avatar" src="https://www.w3schools.com/howto/img_avatar.png" alt="users">
-      <div class=container>
-        <h4><b>Nunzia</b></h4>
-      </div>
+  {#if visible}
+    <div class=container>
+      <h2><b>Claim Id</b></h2>
     </div>
-    <div class="container profile">
-      <img class="avatar" src="https://www.w3schools.com/howto/img_avatar.png" alt="users">
-      <div class=container>
-        <h4><b>Elena</b></h4>
+    <div class="tab1cards">
+      <div class="container profile">
+        <img class="avatar" src="https://www.w3schools.com/howto/img_avatar.png" alt="users">
+        <div class=container>
+          <h4><b>Nunzia</b></h4>
+        </div>
       </div>
-    </div>
-    <div class="container profile">
-      <img class="avatar" src="https://www.w3schools.com/howto/img_avatar.png" alt="users">
-      <div class=container>
-        <h4><b>Tommaso</b></h4>
+      <div class="container profile">
+        <img class="avatar" src="https://www.w3schools.com/howto/img_avatar.png" alt="users">
+        <div class=container>
+          <h4><b>Elena</b></h4>
+        </div>
       </div>
-    </div>
-    <div class="container profile">
-      <img class="avatar" src="https://www.w3schools.com/howto/img_avatar.png" alt="users">
-      <div class=container>
-        <h4><b>Mary</b></h4>
+      <div class="container profile">
+        <img class="avatar" src="https://www.w3schools.com/howto/img_avatar.png" alt="users">
+        <div class=container>
+          <h4><b>Tommaso</b></h4>
+        </div>
       </div>
+      <div class="container profile">
+        <img class="avatar" src="https://www.w3schools.com/howto/img_avatar.png" alt="users">
+        <div class=container>
+          <h4><b>Mary</b></h4>
+        </div>
+      </div>
+    </div>    
+    <div class="container">
+      <p>or else</p>
+      <h3><button class="button" on:click={toggleVisible}>Click to register</button></h3>
     </div>
-  </div>  
-  <form style="display:none">
+  {/if}
+{#if !visible}
+
+<div class="container margin">
+  <form>
     <label>
-      <div>Username</div>
+      <h2 class="margin">Please choose a username to be identified</h2>
       <input
         required
         type="text"
         placeholder="user123"
         v-model="username"
+       
       />
     </label>
   </form>
-  <div class="container">
-    <p>or else</p>
-    <h3><button class="button" onclick="register()">Click to register</button></h3>
-  </div>
+  
+  <Router>
+    <Route path="/insert-phrases"/>
+  </Router>
+  
+  <h3><button class="button" on:click={goToAnotherPage}>Register</button></h3>
 </div>
+{/if}
 
+</div>
+<style>
+  .margin{
+    padding:15px;
+  }
+  input{
+    margin-bottom: 10px;
+    width:100px;
+    height:30px;
+    font-size: 16px;
+  }
+</style>
+
+
+<script>
+  //function that returns the users - connect to db
+  //function that after click on user, navigate to insert-phrases like below
+    //find a way to know if you go to insert-phrases through enrollment or through identification
+  let visible = true;
+
+  function toggleVisible() {
+    visible = !visible
+  }
+
+  import { Router, Link, Route } from 'svelte-routing';  
+  import { navigate } from "svelte-routing";
+  
+  function goToAnotherPage() {
+    navigate("/insert-phrases");
+        };
+        
+</script>
---
+++ demo-app/src/routes/insert-phrases/+page.svelte
@@ -0,0 +1,75 @@
+<div class="title"><h4>Keystroke Enrollment</h4></div>
+
+<div class="container">
+    <div class="container phrase">
+        {#if displayPhrase}
+        <h2>{displayPhrase}</h2>
+        {/if}
+    </div>
+    <button class="button" on:click={appendRandomPhrase}>reload</button>
+    <form>
+      <label>
+        <input
+          required
+          type="text"
+          placeholder="Write the phrase that appears on top here"
+        />
+      </label>
+    </form>
+</div>
+
+<style>
+    
+input {
+    width: 500px;
+    float: left;
+    height: 35px;
+    line-height: 3em;
+    font-size:14pt;
+    border-radius: 5px;
+    border: 2px solid #FAF3DD;
+    
+}
+.button{
+    border-radius: 25px;
+	background-color:#FAF3DD;
+	color: rgb(0, 0, 0);
+	padding: 15px 15px;
+	text-align: center;
+	display: inline-block;
+	font-size: 20px;
+	border: none;
+    margin-bottom: 20px;
+    font-weight: bold;
+}
+
+.container{
+    margin: 20px auto;
+}
+
+.phrase{
+    border: 2px solid #4A7C59;
+    border-radius: 25px;
+    background-color: #FAF3DD;
+    
+}
+h2{
+    font-weight: normal;
+}
+</style>
+
+
+
+<script>
+
+    //function that sends the phrases (and gets values?)
+
+
+    let phrases = ["Hello world","Love live smoke", "Welcome to my phrases' choice", "Ho usato chatGPT"];
+    let displayPhrase = "Who run the world?";
+    function appendRandomPhrase() {
+        const randomIndex = Math.floor(Math.random() * phrases.length);
+        displayPhrase = phrases[randomIndex];
+    }
+
+</script>
